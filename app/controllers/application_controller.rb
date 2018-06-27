b4b7f4e1b251bb12db0ef2a42770fa8f7cd09d3f
brynnebursack <brynnebursack@gmail.com>
2018-06-27 15:50:13 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
-class ApplicationController < Sinatra::Base
+require './config/environment'
+require './app/models/model'
 
+class ApplicationController < Sinatra::Base
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
   end
 
-  get "/" do
-  	erb :index
+  get "/" do 
+  	return erb :index
   end
-end
+  
+  post '/' do
+    @the_fortune=get_fortune
+    @the_user=params[:user] # params is a hash that stores user input, we wanna enter the hash and pull out user  
+    return erb :results 
+  end 
+end 
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,9 @@
-class Model
-  # Replace with Dog clas
-end
+def get_fortune 
+  
+  fortune_array = ["you will drink 20 core water bottles this week", "you will fall in a hole in two days", "you will lose your left shoe this afternoon", "you will eat bok hoy in your near future", "you will get a nosebleed on the metro", "you will come across a field of bok choy"]
+ 
+  return fortune_array.sample
+
+end 
+
+# puts get_fortune 
